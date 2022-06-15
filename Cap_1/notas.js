bbf25e56f68f443bbae62045b099596432dd9a20
Alan Paz  (AIPA) <israel.pazar@gmail.com>
2022-06-14 23:31:19 -0500

    Fin del Cap1 y comienzo Cap2

--- Cap_1/notas.js
+++ Cap_1/notas.js
@@ -10,6 +10,7 @@ let noParen = 100 + 4*11
 let paren= (100+4)*11
 console.log(noParen)
 console.log(paren)
+console.log(" ")
 /* Numeros Especiales. 
 / Infinity, -Infinity  Representa Infinidades.
 / NaN "No es un numero"
@@ -22,6 +23,7 @@ console.log(paren)
 */
 let Salto_Linea= `Esta es la primer linea \nY esta es la segunda.`
 console.log(Salto_Linea)
+console.log(" ")
 
 console.log("Con"+"cat"+"e"+"nar")
 //imprime Concatenar. 
@@ -31,12 +33,14 @@ console.log(`la mitad de 100 es ${100/2}`)
 
 console.log(typeof 4.5) //number. 
 console.log(typeof "x") //string.
+console.log(" ")
 
 //los valores Booleanos solo pueden dar un True o False como valor. estos no se limitan solo a expreciones de numeros si no tambien de strings.
+console.log("Valores Booleanos")
 console.log(3>2) //True.
 console.log(3<2) //False.
 console.log("Aardvark"<"Zoroaster")//True.
-
+console.log(" ")
 /* Operadores de comparacion. 
 / como ya vimos los dos primeros que es menor y mayor que tambien existen los sieguientes: 
 /   >= mayor o igual que. 
@@ -44,5 +48,25 @@ console.log("Aardvark"<"Zoroaster")//True.
 /   ==igual a. 
 /   != diferente a.
 */
-console.log("Itchy"!="Scratchy")
-//True.
+console.log("Operadores de Comparacion")
+console.log("Itchy"!="Scratchy") //True.
+console.log("Manzana"=="Naranja") //False.
+console.log(" ")
+/* Operadores Logicos and or y not. 
+/ && Representa and. 
+/ || Representa or. 
+/ !  Representa not.
+*/ 
+console.log("Operadores Logicos and, or, not")
+console.log(true && false)//false
+console.log(true && true)//true.
+
+console.log(false ||true)//true
+console.log(false ||false)//false
+/* Operador Ternario. 
+/true o false? instruccion 1  : instruccion 2 ,
+/El valor a la izquierda del signo de interrogación “decide” cual de los otros dos valores sera retornado. 
+/Cuando es verdadero, elige el valor de en medio, y cuando es falso, el valor de la derecha.
+*/
+console.log(true ? 1:2)//1
+console.log(false? 1:2)//2
---
+++ Cap_2/notas.js
@@ -0,0 +1,22 @@
+/* Variables let, var, const
+/ let.      es la forma actual de declarar variables. 
+/ var.      forma en que se decalraba una variable de JavaScript previo al 2015. 
+/ const.    representa una constante que apunta al mismo valor el tiempo que viva.  
+*/
+let uno=1,dos=2
+console.log(uno+dos)//3 
+var nombre= "Ayda"
+const saludo= "Hola "
+console.log(saludo + nombre)//Hola Ayda.
+console.log("")
+/* Funciones
+/ prompt , permite la entrada de dialogo al usuario.
+/ console.log , salida de texto.
+*/
+console.log("Funciones")
+//let contraseña= prompt("Introducir contraseña: ")
+//console.log(contraseña)
+console.log(Math.max(2,4)) //4
+console.log(Math.min(2,4)+100)//102
+console.log(" ")
+//Existen la funciones como Number, String y Boolean para convertir los datos en sus respectivos tipos. 
