e632445a0ad57efcc21fcfe60db5882a78052076
Alan Paz  (AIPA) <israel.pazar@gmail.com>
2022-06-16 23:46:53 -0500

    Cap2-Ciclos

--- Cap_2/notas.js
+++ Cap_2/notas.js
@@ -20,3 +20,44 @@ console.log(Math.max(2,4)) //4
 console.log(Math.min(2,4)+100)//102
 console.log(" ")
 //Existen la funciones como Number, String y Boolean para convertir los datos en sus respectivos tipos. 
+/* Ejecucion Condicional. 
+/ If se usa cuando queremos que se ejecute algun codigo si y solo si una cierta condicion se cumple. 
+*/
+let elNumero = 25
+if (!Number.isNaN(elNumero)){
+    console.log("Tu numero es la raiz cuadrada de:  "+elNumero*elNumero)
+}
+if (1+1 ==2) console.log("Es verdad")//Es verdad. 
+// else es una funcion alternativa al if. 
+
+elNumero="hola"
+if (!Number.isNaN(elNumero)){
+    console.log("Tu numero es la raiz cuadrada de:  "+elNumero*elNumero)
+} else{
+    console.log("Ey. Por qué no me diste un número")
+}
+// tambien se puede hacer una forma de switch para el if y else. 
+let numero= 55
+if(numero <10){
+    console.log("Pequeño")
+} else if (numero <100){
+    console.log("Mediano")
+} else{
+    console.log("Grande")
+}
+console.log("")
+/* Ciclos While, Do, For
+/ While crea un ciclo que mientras la expresion sea True se seguira ejecutando. 
+*/
+numero=0 
+while (numero<=12){
+    console.log(numero)
+    numero+=1
+}
+
+let resultado=1 , contador=0
+while (contador <10){
+    resultado=resultado*2
+    contador+=1
+}
+console.log(resultado)
